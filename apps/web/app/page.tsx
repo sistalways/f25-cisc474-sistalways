57ecfa94f9865502081e89f9b1b52a63666d66d0
Girish Sista <vjgcs@udel.edu>
2025-09-12 17:28:13 -0400

    added login page and created folders and files for other pages

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,69 +2,20 @@ import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
 
-type Props = Omit<ImageProps, "src"> & {
-    srcLight: string;
-    srcDark: string;
-};
-
-const ThemeImage = (props: Props) => {
-    const { srcLight, srcDark, ...rest } = props;
-
-    return (
-        <>
-            <Image {...rest} src={srcLight} className="imgLight" />
-            <Image {...rest} src={srcDark} className="imgDark" />
-        </>
-    );
-};
-
-export default function Home() {
+export default function Login() {
     return (
         <div className={styles.page}>
             <main className={styles.main}>
-                <h1>CISC474 Project Starter</h1>
-                <ol>
-                    <li>
-                        Get started by editing{" "}
-                        <code>apps/web/app/page.tsx</code>
-                    </li>
-                    <li>Save and see your changes instantly.</li>
-                </ol>
-
-                <Button appName="web" className={styles.secondary}>
-                    Open alert
-                </Button>
+                <h1>Login with your Credentials</h1>
+                <form className={styles.form}>
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" id="username" name="username" required />
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="password" required />
+                    <Button appName={""}>Login</Button>
+                </form>
             </main>
-            <footer className={styles.footer}>
-                <a
-                    href="https://vercel.com/templates?search=turborepo&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        aria-hidden
-                        src="/window.svg"
-                        alt="Window icon"
-                        width={16}
-                        height={16}
-                    />
-                    Examples
-                </a>
-                <a
-                    href="https://turborepo.com?utm_source=create-turbo"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        aria-hidden
-                        src="/globe.svg"
-                        alt="Globe icon"
-                        width={16}
-                        height={16}
-                    />
-                    Go to turborepo.com →
-                </a>
-            </footer>
         </div>
+        
     );
 }
