623e2d9f481238c8218513aea581a654bee39e8a
Girish Sista <vjgcs@udel.edu>
2025-09-12 20:16:48 -0400

    updated dahsboard page layout

--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -1,22 +1,23 @@
 import React from "react";
 import Link from "next/link";
-import "./dashboard.css"
+import "./dashboard.css";
 
 export default function Dashboard() {
-    return (
-        <div>
-            <main>
-            <h1 style={{alignContent:"center"}}>Dashboard</h1>
+  return (
+    <div className="dashboard-container">
+      
+      <div className="sidebar">
+        <Link href="/dashboard" className="nav-link">Dashboard</Link>
+        <Link href="/courses" className="nav-link">Courses</Link>
+        <Link href="/calendar" className="nav-link">Calendar</Link>
+        <Link href="/profile" className="nav-link">Profile</Link>
+        <Link href="/inbox" className="nav-link">Inbox</Link>
+      </div>
 
-            
-                <Link href="/dashboard" className="nav-link">Dashbaord</Link>
-                <Link href="/courses" className="nav-link">Courses</Link>
-                <Link href="/calendar" className="nav-link">Calendar</Link>
-                
-                <Link href="/profile" className="nav-link">Profile</Link> 
-                <Link href="/inbox" className="nav-link">Inbox</Link> 
-            </main>
-        </div>
-        
-    );
+    
+      <div className="main-content">
+        <h1>This is your Dashboard</h1>
+      </div>
+    </div>
+  );
 }
