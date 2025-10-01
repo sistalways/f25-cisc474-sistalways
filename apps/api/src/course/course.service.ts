730a15636a6baff36f6c8c30113d36b5c87bc866
Girish Sista <vjgcs@udel.edu>
2025-10-01 01:06:43 -0400

    database url fix

--- apps/api/src/course/course.service.ts
+++ apps/api/src/course/course.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service'; // Ensure prisma.service.ts exists in the same directory
-import { Prisma } from '@prisma/client';
+
 
 @Injectable()
 export class CourseService {
