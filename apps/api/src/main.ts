96b3153d14172b37b2f8a3f72c8d31d2e5da11bb
Girish Sista <vjgcs@udel.edu>
2025-10-06 20:06:04 -0400

    fixed error on main.ts in api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: env('FRONTEND_URL'),
+    origin: process.env.FRONTEND_URL,
     methods: ['GET','HEAD','PUT','PATCH','POST','DELETE','OPTIONS'],
     Credentials: true
   });
