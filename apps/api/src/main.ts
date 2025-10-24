b8aab0a4391ec09b3618d2c9092b1f0fe9d252ba
Girish Sista <vjgcs@udel.edu>
2025-10-24 03:34:52 -0400

    cors fix

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,13 +5,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['process.env.FRONTEND_URL','cisc474-ind-frontend.vjgcs.workers.dev','https://f25-cisc474-sistalways.onrender.com','http://localhost:3000/', 'http://localhost:3001/','process.env.VITE_BACKEND_URL'],
+    origin: [process.env.FRONTEND_URL,'cisc474-ind-frontend.vjgcs.workers.dev','https://f25-cisc474-sistalways.onrender.com','http://localhost:3000', 'http://localhost:3001',process.env.VITE_BACKEND_URL],
     methods: ['GET','HEAD','PUT','PATCH','POST','DELETE','OPTIONS'],
-    Credentials: true
+    credentials: true
   });
   const port = process.env.PORT || 3000;
-  const host = process.env.HOST || undefined;
-  await app.listen(port, host);
+  await app.listen(port);
 }
 
 void bootstrap();
