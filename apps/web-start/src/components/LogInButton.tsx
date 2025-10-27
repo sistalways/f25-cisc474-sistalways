4cebe52db09a9a08e9d5617e04f31ae4de6aa6c9
Girish Sista <vjgcs@udel.edu>
2025-10-27 14:57:05 -0400

    fixing redirect issue

--- apps/web-start/src/components/LogInButton.tsx
+++ apps/web-start/src/components/LogInButton.tsx
@@ -11,6 +11,7 @@ const LogInButton = () => {
           authorizationParams: {
             scope: 'read:courses',
             prompt: 'consent',
+            redirect_uri: `${window.location.origin}/home`,
           },
         })
       }
