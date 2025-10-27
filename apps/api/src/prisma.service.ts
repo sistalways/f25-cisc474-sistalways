f4149f697bff7002dad48b385c02a959db405499
Girish Sista <vjgcs@udel.edu>
2025-10-27 02:17:32 -0400

    added authentication table in schema 8

--- apps/api/src/prisma.service.ts
+++ apps/api/src/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '../../../packages/database/generated/client';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
